897c1104afbc15608ba516b772829ec3178c2f6e
Akash Gupta <akashgupta7363@gmail.com>
2023-07-05 20:59:00 +0530

    logo & icons in header

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
-import { Header } from './components';
+import { Header, MainContainer, CreateContainer } from './components';
+import { AnimatePresence } from 'framer-motion';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="w-screen h-auto flex flex-col ">
-      <Header />
-    </div>
+    <AnimatePresence>
+      <div className="w-screen h-auto flex flex-col bg-primary">
+        <Header />
+        <main className="mt-24 p-8 w-full">
+          <Routes>
+            <Route path="/*" element={<MainContainer />} />
+            <Route path="/createitem" element={<CreateContainer />} />
+          </Routes>
+        </main>
+      </div>
+    </AnimatePresence>
   );
 }
 
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,47 +1,55 @@
 import React from 'react';
 import logo from '../assets/img/logo.png';
+import avatar from '../assets/img/avatar.png';
 import { FaShoppingBasket } from 'react-icons/fa';
 
 const Header = () => {
   return (
-    <div
+    <header
       className="fixed z-50 w-screen 
      p-6 px-16 "
     >
-      <div className="hidden md:flex w-full h-full">
+      <div className="hidden md:flex w-full h-full items-center justify-between">
         <div className="flex items-center gap-2">
           <img src={logo} alt="logo" className="w-12 object-cover" />
           <p className="text-headingColor text-lg font-bold">
             Heavenly <br /> Spices
           </p>
         </div>
-        <ul className="flex items-center gap-8 ml-auto ">
-          <li className="text-base text-textColor duration-100 transition-all ease-in-out hover:text-headingColor cursor-pointer">
-            Home
-          </li>
-          <li className="text-base text-textColor duration-100 transition-all ease-in-out hover:text-headingColor cursor-pointer">
-            Menu
-          </li>
-          <li className="text-base text-textColor duration-100 transition-all ease-in-out hover:text-headingColor cursor-pointer">
-            City
-          </li>
-          <li className="text-base text-textColor duration-100 transition-all ease-in-out hover:text-headingColor cursor-pointer">
-            Services
-          </li>
-          <li className="text-base text-textColor duration-100 transition-all ease-in-out hover:text-headingColor cursor-pointer">
-            About us
-          </li>
-        </ul>
-        <div className="relative flex justify-center items-center">
-          <FaShoppingBasket className="text-textColor text-2xl ml-8 cursor-pointer" />
-          <div className="absolute -top-1 -right-1 w-5 h-5 rounded-full bg-cartNumBg  flex items-center justify-center">
-            <p className="text-xs text-white font-semibold">2</p>{' '}
+        <div className="flex items-center gap-8">
+          <ul className="flex items-center gap-8 ml-auto ">
+            <li className="text-base text-textColor duration-100 transition-all ease-in-out hover:text-headingColor cursor-pointer">
+              Home
+            </li>
+            <li className="text-base text-textColor duration-100 transition-all ease-in-out hover:text-headingColor cursor-pointer">
+              Menu
+            </li>
+            <li className="text-base text-textColor duration-100 transition-all ease-in-out hover:text-headingColor cursor-pointer">
+              City
+            </li>
+            <li className="text-base text-textColor duration-100 transition-all ease-in-out hover:text-headingColor cursor-pointer">
+              Services
+            </li>
+            <li className="text-base text-textColor duration-100 transition-all ease-in-out hover:text-headingColor cursor-pointer">
+              About us
+            </li>
+          </ul>
+          <div className="relative flex justify-center items-center">
+            <FaShoppingBasket className="text-textColor text-2xl  cursor-pointer" />
+            <div className="absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg  flex items-center justify-center">
+              <p className="text-xs text-white font-semibold">2</p>{' '}
+            </div>
           </div>
+          <img
+            src={avatar}
+            alt="avatar"
+            className="h-10 w-10 min-h-[40px] min-w-[40px] drop-shadow-xl"
+          />
         </div>
       </div>
 
       <div className="flex md:hidden w-full h-full bg-blue-500 p-4"> </div>
-    </div>
+    </header>
   );
 };
 
